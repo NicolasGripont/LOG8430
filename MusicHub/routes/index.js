bd6e00e85075a46c2c64f8f82b02aeb9cf913d1b
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-10-31 17:25:56 -0400

    Init project

---
+++ MusicHub/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Music Hub' });
+});
+
+router.get('/user/signup', function(req, res, next) {
+    // res.render('view', { title: 'Sign In', layout: 'simple-layout' });
+    res.render('user/signup', {  title: 'Sign Up', layout: 'simple-layout' } );
+});
+
+router.get('/user/signin', function(req, res, next) {
+    // res.render('view', { title: 'Sign In', layout: 'simple-layout' });
+    res.render('user/signin', {  title: 'Sign In', layout: 'simple-layout' } );
+});
+
+module.exports = router;
