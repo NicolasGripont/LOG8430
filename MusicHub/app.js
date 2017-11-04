32cf1f08a0e9e0620154582dc7e95e885fb02f04
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-11-04 16:07:29 -0400

    Modification views

--- MusicHub/app.js
+++ MusicHub/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var expressHandlebars = require('express-handlebars');
 var mongoose = require('mongoose');
 
-var index = require('./routes/index');
+var views = require('./routes/views');
 
 var app = express();
 
@@ -56,7 +56,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', views);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- MusicHub/routes/views.js
+++ MusicHub/routes/views.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Music Hub' });
+    res.render('index', { title: 'Music Hub' });
 });
 
 router.get('/user/signup', function(req, res, next) {
