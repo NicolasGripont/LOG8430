e6f31cc4a149ca90c0b7a45c387ee4ac61a9c0be
KaiserCroc <piebay13@laposte.net>
2017-11-05 16:14:00 -0500

    Add script côté client signup

---
+++ MusicHub/public/javascripts/signup.js
@@ -0,0 +1,41 @@
+$(function() {
+    function sendAJAXRequest(emailFilled, passwordFilled) {
+        $.ajax({
+            url: "/user/signup",
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                email: emailFilled,
+                password: passwordFilled
+            })
+        })
+        .done(function (json) {
+            if (json && json.error) {
+                alert(JSON.stringify(json));
+            }
+            else {
+                window.location.replace("/user/signin");
+                alert("Inscription réussie");
+            }
+        })
+        .fail(function (xhr, status, errorThrown) {
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        });
+    }
+
+    $('form').submit(function (event) {
+        event.preventDefault();
+        var passwordFilled = $("#inputPassword").val();
+        var repasswordFilled = $("#reInputPassword").val();
+        var emailFilled = $("#inputEmail").val();
+
+        if(passwordFilled !== repasswordFilled) {
+            alert("Les mots de passes doivent être identiques.");
+        } else {
+            sendAJAXRequest(emailFilled, passwordFilled);
+        }
+    })
+});
