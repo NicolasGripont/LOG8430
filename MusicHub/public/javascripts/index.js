61cd1f6bafae1d3dbe6e27a2ad3757435edb8815
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-11-25 14:13:20 -0500

    Ajout de la librairie SoundManager2

--- MusicHub/public/javascripts/index.js
+++ MusicHub/public/javascripts/index.js
@@ -61,4 +61,63 @@
     }
 
 
+
+    // soundManager.setup({
+    //     // url: '/path/to/swf-files/',
+    //     // flashVersion: 9, // optional: shiny features (default = 8)
+    //     // optional: ignore Flash where possible, use 100% HTML5 mode
+    //     preferFlash: false,
+    //     onready: function() {
+    //         // Ready to use; soundManager.createSound() etc. can now be called.
+    //         var mySound = soundManager.createSound({
+    //             id: 'Comme prévu - Ninho',
+    //             url: 'http://e-cdn-preview-7.deezer.com/stream/79e6f771ff55d4eb832d3217083428cc-3.mp3',
+    //             autoLoad: true,
+    //             autoPlay: false,
+    //             onload: function() {
+    //                 alert('The sound '+this.id+' loaded!');
+    //             },
+    //             volume: 50
+    //         });
+    //         mySound.play();
+    //     }
+    // });
+
+
+
+    var Player;
+    var players = [];
+    var playerSelector = '.sm2-bar-ui';
+    var playerOptions;
+    var utils;
+
+    playerOptions = {
+        // useful when multiple players are in use, or other SM2 sounds are active etc.
+        stopOtherSounds: true,
+        // CSS class to let the browser load the URL directly e.g., <a href="foo.mp3" class="sm2-exclude">download foo.mp3</a>
+        excludeClass: 'sm2-exclude'
+    };
+
+    soundManager.setup({
+        // trade-off: higher UI responsiveness (play/progress bar), but may use more CPU.
+        html5PollingInterval: 50,
+        preferFlash: false
+    });
+
+    soundManager.onready(function() {
+
+        // var nodes, i, j;
+        //
+        // nodes = utils.dom.getAll(playerSelector);
+        //
+        // if (nodes && nodes.length) {
+        //     for (i = 0, j = nodes.length; i < j; i++) {
+        //         players.push(new Player(nodes[i]));
+        //     }
+        // }
+        $('#sm2-playlist-bd').append('http://e-cdn-preview-7.deezer.com/stream/79e6f771ff55d4eb832d3217083428cc-3.mp3');
+        window.sm2BarPlayers[0].playlistController.refresh();
+    });
+
+
 })();
