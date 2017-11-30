4e869b8697ddb10696aa0b8902a8e990bebdf9d0
LudovicCros <cros_ludovic@hotmail.fr>
2017-11-30 15:20:55 -0500

    Tests en cours sur playlist controller et routes

---
+++ MusicHub/controllers/test/testPlaylist-controller.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+var sinon = require ('sinon');
+var chai = require('chai');
+var Playlist = require('../../models/playlist');
+var PlaylistController = require('../playlist-controller');
+var ConnectorController = require('../connector-controller');
+var Music = require('../../models/music');
+
+
+describe('Playlist-controller', function() {
+	
+	var req;
+	var res;
+    var controller;
+    var email = "admin@admin.com";
+    var pass = "123456";
+    var musicId = "abcd";
+    var musicPlatform = "deezer";
+    var playlistName = "name";
+	var stub = [];
+	
+	beforeEach(function() {
+	    // runs before each test in this block
+		controller = new PlaylistController();
+		res = {
+			status:function(val){return this;},
+			json:function(val){return this;}
+		};
+		req = {
+			params: {
+				playlistName:playlistName
+			},
+			body:{
+				musicId:musicId,
+				musicPlatform:musicPlatform,
+				playlistName:playlistName
+			},
+			session:{
+				destroy:function(){},
+				email:email
+			}
+		};
+	});
+	
+	afterEach(function() {
+	    // runs after each test in this block
+		for(var i=0;i<stub.length;i++) {
+			stub[i].restore();
+		}
+		stub = [];
+	});
+	
+	
+	describe('method createPlaylist', function() {
+        it('should create the user', function(done) {
+        	stub.push(sinon.stub(Playlist.prototype,"save"));
+        	stub.push(sinon.spy(res,"status"));
+        	stub.push(sinon.stub(res,"json"));
+        	stub.push(sinon.spy(Playlist.prototype,"constructor"));
+        	stub[0].callsArgWith(0,null);
+        	stub[2].callsFake(function() {
+        		try {
+        			chai.expect(stub[0].called).to.deep.equal(true);
+        			chai.expect(stub[1].called).to.deep.equal(true);
+        			chai.expect(stub[3].called).to.deep.equal(true);
+        			chai.expect(stub[1].getCall(0).args[0]).to.deep.equal(200);
+        			chai.expect(stub[2].getCall(0).args[0]).to.deep.equal({message:"OK"});
+        			chai.expect(stub[3].getCall(0).args).to.deep.equal([playlistName,email,[]]);
+        			
+            		done();
+        		}catch(err) {
+        			done(err);
+        		}
+        	});
+        	controller.createPlaylist(req,res);
+        });
+	});
+});
