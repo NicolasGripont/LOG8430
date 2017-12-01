779be1ed3b7383dbe94e8389456423044d42e7d9
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-12-01 17:52:24 -0500

    common index

--- MusicHub/common/index.js
+++ MusicHub/common/index.js
@@ -1,11 +1,21 @@
 var config = require("./config.json");
-var deezer = require("./models/deezer-connector");
-var spotify = require("./models/spotify-connector");
-var deezer = require("./models/spotify-connector");
-
-var host = config.host;
-var port = config.port;
+var DeezerConnector = require("./models/deezer-connector");
+var SpotifyConnector = require("./models/spotify-connector");
+var Music = require("./models/music");
+var Playlist = require("./models/playlist");
+var Settings = require("./models/settings");
+var User = require("./models/user");
+var PlaylistDB = require("./models_db/playlist");
+var SettingsDB = require("./models_db/settings");
+var UserDB = require("./models_db/user");
 
 module.exports.config = config;
-module.exports.host = host;
-module.exports.port = port;
+module.exports.DeezerConnector = DeezerConnector;
+module.exports.SpotifyConnector = SpotifyConnector;
+module.exports.Music = Music;
+module.exports.Playlist = Playlist;
+module.exports.Settings = Settings;
+module.exports.User = User;
+module.exports.PlaylistDB = PlaylistDB;
+module.exports.SettingsDB = SettingsDB;
+module.exports.UserDB = UserDB;
