3e85cd8ac616e64d42850c6072461ebc50332481
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-12-03 15:41:49 -0500

    fix index.js

--- MusicHub/common/index.js
+++ MusicHub/common/index.js
@@ -1,4 +1,6 @@
 var config = require("./config.json");
+var DeezerConnectorParams = require("./models/deezer-connector-params");
+var SpotifyConnectorParams = require("./models/spotify-connector-params");
 var DeezerConnector = require("./models/deezer-connector");
 var SpotifyConnector = require("./models/spotify-connector");
 var Music = require("./models/music");
@@ -10,6 +12,8 @@ var SettingsDB = require("./models_db/settings");
 var UserDB = require("./models_db/user");
 
 module.exports.config = config;
+module.exports.DeezerConnectorParams = DeezerConnectorParams;
+module.exports.SpotifyConnectorParams = SpotifyConnectorParams;
 module.exports.DeezerConnector = DeezerConnector;
 module.exports.SpotifyConnector = SpotifyConnector;
 module.exports.Music = Music;
